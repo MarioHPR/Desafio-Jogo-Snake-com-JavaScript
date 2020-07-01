55510e84111920f69818f1e7ea7a613d17f4fcaf
Mario Henrique Pereira da Rosa <mariopereira398@gmail.com>
2020-07-01 16:59:15 -0300

    Alteração das funções para arrow function

--- script.js
+++ script.js
@@ -12,33 +12,31 @@ snake[0] = {
 
 let direction = "right";
 
-function sortear() {
-    return Math.floor(Math.random() * 15 + 1) * box;
-}
+const sortear = () => Math.floor(Math.random() * 15 + 1) * box;
 
 let food = {
     x : sortear(),
     y : sortear()
 }
 
-function criarBG() {
+const criarBG = () => {
     context.fillStyle = "Lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarSnake() {
+const criarFood = () => {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+const criarSnake = () => {
     for(let i = 0; i < snake.length; i++ ){
         context.fillStyle = "black";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function criarFood() {
-    context.fillStyle = "red";
-    context.fillRect(food.x, food.y, box, box);
-}
-
-function update(event) {
+const update = (event) => {
     if (event.keyCode == 37 && direction != "right") direction = "left";
     if (event.keyCode == 38 && direction != "down" ) direction = "up";
     if (event.keyCode == 39 && direction != "left" ) direction = "right";
